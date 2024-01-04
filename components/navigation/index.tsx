c3b563f750c45a87668417ccf8c35056fbbf68cf
jing zhou <laorejadesofi@jingmatoMBP.technicolor.net>
2024-01-04 13:21:43 +0100

    minor fixes and changes

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -28,11 +28,9 @@ export default function Navbar() {
                             </span>
                         </Link>
                         <Link href="/faucet" className="px-4 py-2">Claim</Link>
-                        <Link href="/mint" className="px-4 py-2">Mint</Link>
-                        <Link href="/stake" className="px-4 py-2">Stake</Link>
+                        <Link href="/mint" className="px-4 py-2">Releases</Link>
+                        <Link href="/stake" className="px-4 py-2">Staking Pools</Link>
                         <Link href="/about" className="px-4 py-2">About</Link>
-                        <Link href="/store" className="px-4 py-2">Store</Link>
-
                     </li>
 
                     <li className='flex gap-1 w-fit justify-end self-center'>
--- helpers/converter.ts
+++ helpers/converter.ts
@@ -1,6 +1,7 @@
 import { ethers } from "ethers"
 
 export function parseAmount(amount: any) {
+    if(!amount) return 0
     return parseFloat(ethers.formatEther(amount.toString())).toLocaleString()
 }
 
