ed01abd38c088b0238f9fad5b0b775fad78ba6ef
kennethsolidity <kenneth@shuttlelabs.xyz>
2025-01-03 19:07:47 +0100

    Added about mint and faucet to public routes

--- routes.ts
+++ routes.ts
@@ -3,34 +3,37 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes:string[] = [
-    "/",
-    "/auth/new-verification",
-    "/voucher-password"
-  ];
-  
-  /**
-   * An array of routes that are used for authentication
-   * These routes will redirect logged in users to /settings
-   * @type {string[]}
-   */
-  export const authRoutes:string[] = [
-    "/auth/login",
-    "/auth/signup",
-    "/auth/error",
-    "/auth/reset",
-    "/auth/new-password"
-  ];
-  
-  /**
-   * The prefix for API authentication routes
-   * Routes that start with this prefix are used for API authentication purposes
-   * @type {string}
-   */
-  export const apiAuthPrefix: string = "/api/auth";
-  
-  /**
-   * The default redirect path after logging in
-   * @type {string}
-   */
-  export const DEFAULT_LOGIN_REDIRECT:string = "/dashboard";
+export const publicRoutes: string[] = [
+  "/",
+  "/faucet",
+  "/mint",
+  "/about",
+  "/auth/new-verification",
+  "/voucher-password"
+];
+
+/**
+ * An array of routes that are used for authentication
+ * These routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+export const authRoutes: string[] = [
+  "/auth/login",
+  "/auth/signup",
+  "/auth/error",
+  "/auth/reset",
+  "/auth/new-password"
+];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+export const apiAuthPrefix: string = "/api/auth";
+
+/**
+ * The default redirect path after logging in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT: string = "/dashboard";
